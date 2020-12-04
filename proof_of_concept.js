e2f53a276faac6fe1ff18708af97dfc09ee3feaa
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2020-12-04 16:45:34 +0100

    Package.json hinzugefügt und proof_of_conept für Spotify API

---
+++ proof_of_concept.js
@@ -0,0 +1,34 @@
+var request = require('request'); // "Request" library
+
+var client_id = 'YourId'; // Your client id
+var client_secret = 'YourSecret'; // Your secret
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/search?q=tiptoe&type=track',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body.tracks.items[0]);
+    });
+  }
+});
