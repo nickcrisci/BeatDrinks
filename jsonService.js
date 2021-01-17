e56437a97f233b77287ac587888fb3ba9eeb18b7
Sebastian Brock <sebastian.brock@outlook.de>
2021-01-17 17:30:17 +0100

    jsonService.js added

---
+++ jsonService.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+const { userInfo } = require('os');
+
+//var data = JSON.parse(fs.readFileSync('Cocktails.json','utf8'));
+
+const writefile = (callback) =>{
+    var data = JSON.parse(fs.readFileSync('Cocktails.json','utf8'));
+    return callback(null, data);
+}
+
+//Überprüfe, ob ein Cocktail eine spezifische Zutat beinhaltet
+const checkproperty = (drinknr,property,callback) =>{
+    var data = JSON.parse(fs.readFileSync('Cocktails.json','utf8'));
+    //console.log(data.cocktails[drinknr].ingredients[0][property]);
+    for(var i = 0; i< data.cocktails[drinknr].ingredients.length;i++){
+        //console.log(typeof data.cocktails[drinknr].ingredients[i][property]);
+        if(typeof data.cocktails[drinknr].ingredients[i][property] != "undefined"){
+            return callback(null, true);
+        }
+    }
+    return callback(null, false);
+}
+
+const preference = (want,hate,callback) => {
+    var data = JSON.parse(fs.readFileSync('Cocktails.json','utf8'));
+    var ergebnis = [];
+    
+    if(want == null){
+        ergebnis = data.cocktails;
+    } else{
+        for(var i = 0; i< data.cocktails.length; i++){
+            var ingredientexists = false;
+            for(var t = 0; t< data.cocktails[i].ingredients.length; t++){
+                //console.log(data.cocktails[0].ingredients[0].Wodka);
+                //return callback(null,data.cocktails[i].ingredients[t]);
+                for(var a = 0; a < want.length; a++){
+                    checkproperty(i,want[a],(err,result)=>{
+                        if(result == true) ingredientexists = true;
+                    }) 
+                }
+            }
+           if(ingredientexists) ergebnis.push(data.cocktails[i]);
+            
+        }
+    }
+    
+    
+    if(hate == null){
+        return callback(null,ergebnis);
+    } else{
+        var i = 0;
+        var deleted = false;
+        while(typeof ergebnis[i] !== "undefined"){
+            deleted = false;
+                for(var a = 0; a< hate.length; a++){
+                    if(!deleted){
+                        
+                        checkproperty(i,hate[a],(err,result)=>{                       
+                            
+                            if(result = true){
+                                //console.log(ergebnis[i]);
+                                ergebnis.splice(i,1);
+                                deleted = true;
+                                
+                            
+                            }
+                        })
+                    }
+                }
+            i++;
+        }
+    }
+    return callback(null, ergebnis);
+}
+
+
+function main(){
+    //console.log(data.cocktails[1]);
+    var pref = ["Wodka"];
+    var hass = ["Gin"];
+
+    /*
+    writefile((err,result)=>{
+        console.log(result);
+    })
+    */
+
+    
+    preference(pref,hass,(err,result)=>{
+        console.log(result);
+    })
+    
+
+    /*
+    checkproperty(2,"Gin",(err,result)=>{
+        console.log(result);
+    })
+    */
+    
+    /*
+   checkproperty(0,"Bitterlemon",(err,result)=>{
+       console.log(result);
+   })
+   */
+      
+} 
+
+main();
