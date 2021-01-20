9bc17dec94ddbe663bc83305db4e7fb76885e2bc
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2021-01-20 11:30:01 +0100

    Struktur gegeben und dependencies aktualisiert

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 "use strict";
 
+const http = require('http')
 
+const { processenv } = require('processenv');
+
+const app = require('./lib/getApp.js');
+
+const server = http.createServer(app)
+
+const port = processenv('PORT', 3000);
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
