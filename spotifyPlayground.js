022440c4b043c1f7d34a9d2c52d6d193b402ac57
Paul-Johne <paul_andre.johne@smail.th-koeln.de>
2021-02-02 17:05:48 +0100

    added first version of get alpha_getRecommendationBasedOnValues and redesign code structure

--- spotifyPlayground.js
+++ spotifyPlayground.js
@@ -1,15 +1,24 @@
 'use strict';
 
-// I will try to code the cases with express instead of request - Paul
-const express = require('express');
-
-const app = express();
+/* I will stick with request since it's related to python request which is easier to overlook for Nick 
+   Nevertheless, express should still be used for our own server. - Paul                               */
+const request = require('request');
 
 /* developer key should be set to undefined and will only be change due to setSpotifyClientInfo */
 let client_id = '785df100c7994a0da2abeb60862fba8f';
 let client_secret = '158c44c48bd54f458cb3ec14b4fd432a';
 
-/* sets local parameters for further operations */
+/* creates object for spotify token-request */
+class AuthForToken {
+    constructor(current_id = client_id, current_secret = client_secret, grantType = 'client_credentials', json = true) {
+        this.url = 'https://accounts.spotify.com/api/token';
+        this.headers = { Authorization: 'Basic ' + (new Buffer.from(current_id + ':' + current_secret).toString('base64')) };
+        this.form = { grant_type: grantType }
+        this.json = json;
+    };
+};
+
+/* sets local parameters for further operations => somehow replaced with class AuthForToken*/
 const setSpotifyClientInfo = (userApiKey = null, callback) => {
     if (!userApiKey) {
         return callback(new Error('Spotify API Key: Key is missing'), null);
@@ -36,27 +45,25 @@ const checkSpotifyClientInfo = (callback) => {
     };
 };
 
-/* deprecated version */
-let authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-        /* should be dynamic */
-        Authorization: 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
-    },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
-};
-
-/* creates object for spotify token-request */
-class AuthForToken {
-    constructor(current_id = client_id, current_secret = client_secret, grantType = 'client_credentials', json = true) {
-        this.url = 'https://accounts.spotify.com/api/token';
-        this.headers = { Authorization: 'Basic ' + (new Buffer.from(current_id + ':' + current_secret).toString('base64')) };
-        this.form = { grant_type: grantType }
-        this.json = json;
-    };
+function alpha_getRecommendationBasedOnValues(authOptions) {
+    console.log('\x1b[35m\nalpha_getRecommendationBasedOnValues was invoked.\x1b[0m')
+    // ISSUE: request.post isn't telling, that API Key is wrong => simply skips action
+    request.post(authOptions, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            var token = body.access_token;
+            var options = {
+            // ISSUE: must be abstract
+            url: 'https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_tracks=0c6xIDDpzE81m2q797ordA&target_acousticness=1.0&min_energy=0.2&target_loudness=0.2&min_popularity=50&min_valence=0.5&max_valence=1.0&target_valence=0.8&market=AU',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
+            json: true
+            };
+            request.get(options, (error, response, body) => {
+                console.log(body.tracks[0]);
+            });
+        };
+    });
 };
 
 // INSERT NEW CODE ABOVE HERE
@@ -69,15 +76,16 @@ module.exports = {
  *              TESTING BELOW THIS BOX                  *
  *******************************************************/
 
-
+/*
 setSpotifyClientInfo({id: 'adawawfafaw121a', secret: '21dib2l23hjawddadeggtjkr'}, (err, data) => {
     if (err) { console.log(err.message); };
     
     console.log(
-        'Spotify API Key: Key was set to:\n',
+        'Notification: Spotify API Key was set to:\n',
         `\x1b[1m client_id: ${data.client_id}\n`,
         ` client_secret: ${data.client_secret}\x1b[0m`);
 });
+*/
 
 console.log(`\n\x1b[35mBuffer-Tests:\x1b[0m`)
 let myBufferToBase64 = new Buffer.from(client_id + ':' + client_secret).toString('base64');
@@ -89,10 +97,13 @@ console.log(myBufferedOII.toString())
 console.log(myBufferedOII.toString('base64'));
 
 console.log(`\n\x1b[35mAuthOptions-Tests:\x1b[0m`);
-console.log(authOptions);
-let currentUserAuth = new AuthForToken(client_id, client_secret, 'client_credentials', true);
-console.log(currentUserAuth);
-currentUserAuth.headers.Authorization = 'I can finally change every aspect of the AuthOptions for Spotify';
-console.log(currentUserAuth);
 let defaultUserAuth = new AuthForToken();
+let someChangedUserAuth = new AuthForToken(client_id, client_secret, 'client_credentials', true);
+someChangedUserAuth.headers.Authorization = 'I can finally change every aspect of the AuthOptions for Spotify';
 console.log(defaultUserAuth);
+console.log(someChangedUserAuth);
+
+alpha_getRecommendationBasedOnValues(new AuthForToken()); // works
+alpha_getRecommendationBasedOnValues(defaultUserAuth); // works
+alpha_getRecommendationBasedOnValues(someChangedUserAuth); // no feedback !!!
+alpha_getRecommendationBasedOnValues(); // no feedback !!!
