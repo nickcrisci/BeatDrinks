b7941cfa265c041d1a028cdfc687d5467f9ed54c
Paul-Johne <paul_andre.johne@smail.th-koeln.de>
2021-02-15 06:26:00 +0100

    IMPORTANT: You need to replace your entire local data with this version! (repository was fixed)

--- spotifyPlayground.js
+++ spotifyPlayground.js
@@ -2,13 +2,14 @@
 
 /* I will stick with request since it's related to python request which is easier to overlook for Nick 
    Nevertheless, express should still be used for our own server. - Paul                               */
-const request = require('request');
+const request = require('request'),
+      generateUrl = require('./lib/modules/urlGenerator');
 
 /* developer key should be set to undefined and will only be change due to setSpotifyClientInfo */
 let client_id = '785df100c7994a0da2abeb60862fba8f';
 let client_secret = '158c44c48bd54f458cb3ec14b4fd432a';
 
-/* creates object for spotify token-request */
+/* object for spotify token-request */
 class AuthForToken {
     constructor(current_id = client_id, current_secret = client_secret, grantType = 'client_credentials', json = true) {
         this.url = 'https://accounts.spotify.com/api/token';
@@ -18,8 +19,8 @@ class AuthForToken {
     };
 };
 
-/* sets local parameters for further operations => somehow replaced with class AuthForToken*/
-const setSpotifyClientInfo = (userApiKey = null, callback) => {
+/* replaces/sets entire client data ; undefined userApiKey will possess default have */
+const replaceSpotifyClientInfo = (userApiKey = null, callback) => {
     if (!userApiKey) {
         return callback(new Error('Spotify API Key: Key is missing'), null);
     };
@@ -45,25 +46,50 @@ const checkSpotifyClientInfo = (callback) => {
     };
 };
 
-function alpha_getRecommendationBasedOnValues(authOptions) {
-    console.log('\x1b[35m\nalpha_getRecommendationBasedOnValues was invoked.\x1b[0m')
-    // ISSUE: request.post isn't telling, that API Key is wrong => simply skips action
+/* returns a certain ammount of tracks based on genres */
+// ISSUE: deprecated parameters !!!
+function spotifyRecommendationWithGenre_alpha(authOptions, musicPreferences, limitMarketGenres) {
+    console.log('\x1b[35m\nspotifyRecommendationWithGenre was invoked.\x1b[0m')
+
     request.post(authOptions, (error, response, body) => {
         if (!error && response.statusCode === 200) {
+            /* Token is always different, therefore the given pattern is necessary */
             var token = body.access_token;
             console.log(`Token : ${token}`);
             var options = {
-            // ISSUE: must be abstract
-                url: 'https://api.spotify.com/v1/recommendations?seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_tracks=0c6xIDDpzE81m2q797ordA&target_acousticness=1.0&min_energy=0.2&target_loudness=0.2&min_popularity=50&min_valence=0.5&max_valence=1.0&target_valence=0.8&market=AU',
+                url: generateUrl(musicPreferences, limitMarketGenres, (err, result) => {
+                    if (!err) {
+                        return result;
+                    } else {
+                        return `/error/${err}` // must be improved
+                    }
+                }),
                 headers: {
                     'Authorization': 'Bearer ' + token
                 },
             json: true
             };
+            /* Properties of callback attributes:
+               1) error: not clear
+               2) response: metadata and representation/body
+               3) body: tracks (current standard: 20), seeds (length: 2 ???) */
             request.get(options, (error, response, body) => {
-                //console.log(body.tracks[0]);
-                console.log("Erfolg")
+                // real logic must be implemented here
+                if (error !== null || error !== undefined) {
+                    console.log('Number of Tracks: ' + body.tracks.length);
+                    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+                    console.log('Seeds-Length: ' + body.seeds.length);
+                    console.log(Object.getOwnPropertyNames(body.seeds));
+                    console.log(Object.getOwnPropertyNames(body.seeds[0]));
+                    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+                    console.log(body.tracks[0]);
+                } else {
+                    return error; // must be improved
+                };
             });
+        } else {
+            console.log("Token request was't successful! Statuscode: " + response.statusCode);
+            return error; // must be improved
         };
     });
 };
@@ -71,8 +97,8 @@ function alpha_getRecommendationBasedOnValues(authOptions) {
 // INSERT NEW CODE ABOVE HERE
 
 module.exports = {
-    // interfaces for other project parts
-}
+    spotifyRecommendationWithGenre_alpha
+};
 
 /********************************************************
  *              TESTING BELOW THIS BOX                  *
@@ -105,8 +131,4 @@ someChangedUserAuth.headers.Authorization = 'I can finally change every aspect o
 console.log(defaultUserAuth);
 console.log(someChangedUserAuth);
 
-//alpha_getRecommendationBasedOnValues(new AuthForToken()); // works
-alpha_getRecommendationBasedOnValues(defaultUserAuth);
-alpha_getRecommendationBasedOnValues(defaultUserAuth); // works
-//alpha_getRecommendationBasedOnValues(someChangedUserAuth); // no feedback !!!
-//alpha_getRecommendationBasedOnValues(); // no feedback !!!
+spotifyRecommendationWithGenre_alpha(defaultUserAuth, [], {limit: 6, market: "DE", genre_seeds: ["disney", "movies"]});
