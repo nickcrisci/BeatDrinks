31e1af9c7d81ab4c74949b71ed29059c03f4981f
Paul-Johne <paul_andre.johne@smail.th-koeln.de>
2021-02-17 22:04:43 +0100

    renamed app.js to server.js | renamed spotifyPlaground.js to getSpotifyRecommendation.js | modified Datenformate.txt

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 "use strict";
 
 const http = require('http'),
-    path = require("path");
+      path = require("path");
 
 const { processenv } = require('processenv');
 
 const app = require(path.join(__dirname, "lib", "getApp.js"));
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const port = processenv('PORT', 3000);
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`)
-})
+});
